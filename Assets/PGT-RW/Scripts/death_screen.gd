b0aa5422367180a024a2f07e38a2560489833f0b
EverKingTransmiGamers <everkingtg2k@outlook.com>
2025-06-06 13:08:50 -0500

    Deleted files v1.0

--- Assets/PGT-RW/Scripts/death_screen.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-
-func _on_retry_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/mundo.tscn")
-
-func _on_back_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/menu_inicio.tscn")
